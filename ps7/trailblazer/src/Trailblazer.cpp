9be2add83a9e261e9142d8be1c39eef664cd1183
Josh Sullivan <jasulliv@stanford.edu>
2018-06-04 22:27:07 -0700

    Completed BFS, Dijkstra, and A*

--- ps7/trailblazer/src/Trailblazer.cpp
+++ ps7/trailblazer/src/Trailblazer.cpp
@@ -1,8 +1,13 @@
-// This is the CPP file you will edit and turn in.
-// Also remove these comments here and add your own, along with
-// comments on every function and on complex code sections.
-// TODO: write comment header for this file; remove this comment
+/*
+ * This sourcecode file implements three different graph search algorithms:
+ * [1] Breadth-First Search (BFS) using a standard queue data structure.
+ * [2] Dijkstra's algorithm using a priority queue data structure.
+ * [3] A* algorithm using a priority queue data structure and a heuristic metric
+ *     composed of the minimum feasible time to traverse the straight-line distance
+ *     between the current path endpoint and the desired final endpoint.
+*/
 
+#include "pqueue.h"
 #include "queue.h"
 #include "hashset.h"
 #include "Trailblazer.h"
@@ -12,7 +17,16 @@ static const double SUFFICIENT_DIFFERENCE = 0.2;
 
 // Function prototypes (instead of adding to Trailblazer.h)
 void visitNode(RoadNode* node, HashSet<string>& visitedNodes);
-void enqueueNeighbors(Path& currPath, RoadNode* node, HashSet<string>& visitedNodes, const RoadGraph& graph);
+void enqueueNeighbors(Queue<Path>& pathQ, const Path& currPath, RoadNode* node,
+                      const HashSet<string>& visitedNodes, const RoadGraph& graph);
+void enqueueNeighbors(PriorityQueue<Path>& pathQ, double& currCost, RoadNode* end, const bool useAstar, const Path& currPath,
+                      RoadNode* node, const HashSet<string>& visitedNodes, const RoadGraph& graph);
+double computeCost(const Path& currPath, const RoadGraph& graph);
+double computeHeuristic(RoadNode* start, RoadNode* end, const RoadGraph& graph);
+
+/*
+ * Helper function for visiting a RoadNode in the trailblazing algorithms.
+*/
 
 void visitNode(RoadNode* node, HashSet<string>& visitedNodes){
 
@@ -21,7 +35,13 @@ void visitNode(RoadNode* node, HashSet<string>& visitedNodes){
 
 }
 
-void enqueueNeighbors(Path& currPath, RoadNode* node, HashSet<string>& visitedNodes, Queue<Path>& pathQ, const RoadGraph& graph){
+/*
+ * Helper function for enqueueing neighbors of a node in preparation for path exploration.
+ * Note that this function is overloaded to be used with a Queue data structure for BFS, or
+ * a PriorityQueue data structure for Dijkstra and A*.
+*/
+void enqueueNeighbors(Queue<Path>& pathQ, const Path& currPath, RoadNode* node,
+                      const HashSet<string>& visitedNodes, const RoadGraph& graph){
 
     Path newPath;
     for (RoadNode* neighbor : graph.neighborsOf(node)){
@@ -29,13 +49,65 @@ void enqueueNeighbors(Path& currPath, RoadNode* node, HashSet<string>& visitedNo
         if (!visitedNodes.contains(neighbor->nodeName())){
             newPath = currPath;
             newPath.add(neighbor);
-            neighbor->setColor(Color::YELLOW);
             pathQ.enqueue(newPath);
+            neighbor->setColor(Color::YELLOW);
             newPath.clear();
         }
     }
 }
 
+void enqueueNeighbors(PriorityQueue<Path>& pathQ, double& currCost, RoadNode* end, const bool useAstar,
+                      const Path& currPath, RoadNode* node, const HashSet<string>& visitedNodes, const RoadGraph& graph){
+
+    Path newPath;
+    double updatedCost;
+    double heuristicCost;
+    // Computing the prior heuristic cost so that we can remove it from the new cost computations
+    double priorHeuristicCost = useAstar * computeHeuristic(node, end, graph);
+
+    for (RoadNode* neighbor : graph.neighborsOf(node)){
+
+        if (!visitedNodes.contains(neighbor->nodeName())){
+
+            newPath = currPath;
+            newPath.add(neighbor);
+
+            // Taking current cost, removing the prior heuristic cost and then adding the new edge cost
+            updatedCost = currCost - priorHeuristicCost + computeCost(newPath, graph);
+            heuristicCost = useAstar * computeHeuristic(newPath[newPath.size() - 1], end, graph);
+
+            // Enqueuing with updated cost and the new hueristic cost
+            pathQ.enqueue(newPath, updatedCost + heuristicCost);
+            neighbor->setColor(Color::YELLOW);
+
+            newPath.clear();
+        }
+    }
+}
+
+/*
+ * Helper function for computing the edge cost assoicated with the newest to entries to a path.
+*/
+double computeCost(const Path& currPath, const RoadGraph& graph){
+
+    size_t endIdx = currPath.size() - 1;
+    double cost = graph.edgeBetween(currPath[endIdx - 1], currPath[endIdx])->cost();
+
+    return cost;
+}
+
+/*
+ * Helper function for computing the heuristic cost from the last node in the current path
+ * to the desired end node.
+*/
+double computeHeuristic(RoadNode* start, RoadNode* end, const RoadGraph& graph){
+
+    return graph.crowFlyDistanceBetween(start, end) / graph.maxRoadSpeed();
+}
+
+/*
+ * Implementing a Breadth-First Search (BFS) using a standard queue data structure.
+*/
 Path breadthFirstSearch(const RoadGraph& graph, RoadNode* start, RoadNode* end) {
 
 
@@ -60,7 +132,7 @@ Path breadthFirstSearch(const RoadGraph& graph, RoadNode* start, RoadNode* end) 
                 return currPath;
             }
 
-            enqueueNeighbors(currPath, currLast, visitedNodes, pathQ, graph);
+            enqueueNeighbors(pathQ, currPath, currLast, visitedNodes, graph);
 
         }
     }
@@ -68,20 +140,82 @@ Path breadthFirstSearch(const RoadGraph& graph, RoadNode* start, RoadNode* end) 
     return {};
 }
 
+/*
+ * Implementing Dijkstra's algorithm using a priority queue data structure.
+*/
 Path dijkstrasAlgorithm(const RoadGraph& graph, RoadNode* start, RoadNode* end) {
-    /* TODO: Delete the following lines and implement this function! */
-    (void) graph;
-    (void) start;
-    (void) end;
+
+    Path pathVec;
+    pathVec.add(start);
+
+    PriorityQueue<Path> pathQ;
+    pathQ.enqueue(pathVec, 0.0);
+
+    HashSet<string> visitedNodes;
+
+    while (!pathQ.isEmpty() and !visitedNodes.contains(end->nodeName())){
+
+        double currCost = pathQ.peekPriority();
+        Path currPath = pathQ.dequeue();
+        RoadNode* currLast = currPath[currPath.size() - 1];
+
+        if (!visitedNodes.contains(currLast->nodeName())){
+
+            visitNode(currLast, visitedNodes);
+
+            if (currLast->nodeName() == end->nodeName()) {
+                return currPath;
+            }
+
+            bool useAstar = false;
+            enqueueNeighbors(pathQ, currCost, end, useAstar, currPath, currLast, visitedNodes, graph);
+        }
+    }
+
     return {};
 }
 
-Path aStar(const RoadGraph& graph, RoadNode* start, RoadNode* end) {
-    /* TODO: Delete the following lines and implement this function! */
-    (void) graph;
-    (void) start;
-    (void) end;
+/*
+ * Implementing the A* algorithm using a priority queue data structure with a heuristic for
+ * underestimating the time-to-traverse cost computed using the straightline distance between
+ * two nodes divided by the maximum allowable speed on the graph.  Note that there is the
+ * option for ignoring a specified edge in this implementation.
+*/
+Path aStarHelper(const RoadGraph& graph, RoadNode* start, RoadNode* end) {
+
+    Path pathVec;
+    pathVec.add(start);
+
+    PriorityQueue<Path> pathQ;
+    pathQ.enqueue(pathVec, computeHeuristic(start, end, graph));
+
+    HashSet<string> visitedNodes;
+
+    while (!pathQ.isEmpty() and !visitedNodes.contains(end->nodeName())){
+
+        double currCost = pathQ.peekPriority();
+        Path currPath = pathQ.dequeue();
+        size_t endIdx = currPath.size() - 1;
+
+        RoadNode* currLast = currPath[endIdx];
+
+        if (!visitedNodes.contains(currLast->nodeName())){
+
+            visitNode(currLast, visitedNodes);
+
+            if (currLast->nodeName() == end->nodeName()) {
+                return currPath;
+            }
+
+            bool useAstar = true;
+            enqueueNeighbors(pathQ, currCost, end, useAstar, currPath, currLast, visitedNodes, graph);
+        }
+    }
+
     return {};
+}
+
+Path aStar(const RoadGraph&graph, RoadNode* start, RoadNode* end) {
 }
 
 Path alternativeRoute(const RoadGraph& graph, RoadNode* start, RoadNode* end) {
